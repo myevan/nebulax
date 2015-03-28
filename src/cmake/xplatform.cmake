cb6d5a3344ab535ed1e72935fa002485b67e18a8
Song Young-jin <myevan80@gmail.com>
2015-03-29 01:32:32 +0900

    + xplatform dir

--- src/cmake/xplatform.cmake
+++ src/cmake/xplatform.cmake
@@ -1,5 +1,3 @@
-set(CMAKE_DEBUG_POSTFIX "_d")
-
 if(NOT CMAKE_BUILD_TYPE)
     set(CMAKE_BUILD_TYPE "Debug")
     message("No CMAKE_BUILD_TYPE specified, defaulting to ${CMAKE_BUILD_TYPE}")
@@ -11,3 +9,6 @@ if (APPLE)
     mark_as_advanced(CORE_FOUNDATION_LIBRARY)
     set(SYSTEM_LIBS ${CORE_FOUNDATION_LIBRARY})
 endif()
+
+set(CMAKE_DEBUG_POSTFIX "_d")
+set(XPLATFORM_DIR "${CMAKE_SOURCE_DIR}/../../xplatform/platforms/${XPLATFORM_NAME}")
