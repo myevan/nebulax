b114070227eb715efbe512cb479a545a7bcfdcc9
Song Young-jin <myevan80@gmail.com>
2015-03-28 11:43:50 +0900

    + posix precompiled headers: unistd.h, limits.h @ foundation

--- src/foundation/core/posix/precompiled.h
+++ src/foundation/core/posix/precompiled.h
@@ -3,14 +3,17 @@
 #define CORE_POSIX_PRECOMPILED_H
 //------------------------------------------------------------------------------
 /**
-    @file core/win32/precompiled.h
+    @file core/posix/precompiled.h
     
     Contains precompiled headers on the Posix platform.
     
     (C) 2007 Radon Labs GmbH
+    (C) 2015 myevan 
 */
 
 // crt headers
+#include <limits.h>
+#include <unistd.h>
 #include <math.h>
 #include <stdlib.h>
 #include <stdarg.h>
